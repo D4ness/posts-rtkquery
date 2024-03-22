e094211c0d25c0a7332bd09d1a6c0ffc850ec0b7
D4ness <lyakhov016@gmail.com>
2024-03-22 16:23:15 +0300

    fix router path

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,12 @@ import {Provider} from 'react-redux'
 import {setupStore} from './shared/lib/store';
 
 const store = setupStore();
+const basename = process.env.NODE_ENV === 'production' ? '/posts-rtkquery' : '/';
 
 const container: HTMLElement = document.getElementById("root")
 createRoot(container).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
             <Suspense fallback="Loading...">
                 {/*<ThemeProvider>*/}
                 <Provider store={store}>
