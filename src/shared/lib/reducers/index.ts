572010a830b61be0a840cc81db15ebdcbf7bfda2
D4ness <lyakhov016@gmail.com>
2024-03-22 16:24:27 +0300

    Updates

--- src/shared/lib/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import {postAPI} from '../services/PostService';
-
-export const rootReducer = combineReducers({
-  [postAPI.reducerPath]: postAPI.reducer,
-})
-
-export type RootState = ReturnType<typeof rootReducer>
-
